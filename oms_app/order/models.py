196e02de97c109ec215808010484c6fa219c69c2
zhanghh <519241620@qq.com>
2019-06-01 21:32:19 +0800

    提交order和orderItem的model

--- oms_app/order/models.py
+++ oms_app/order/models.py
@@ -1,13 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+import uuid
 
 
-class product(models.Model):
-    product_classification = models.CharField(max_length=100)
-    name = models.CharField(max_length=200)
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, auto_created=True, default=uuid.uuid4, editable=False)
+    order_no = models.CharField(max_length=64)
+    create_time = models.DateTimeField(auto_now=True, auto_now_add=True)
+    create_by = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.order_no
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, auto_created=True, default=uuid.uuid4, editable=False)
+    classic = models.CharField(max_length=32)
+    name = models.CharField(max_length=512)
+    sku = models.CharField(max_length=32)
     price = models.FloatField()
 
     def __str__(self):
         return self.name
+
+
+class OrderItem(models.Model):
+    id = models.UUIDField(primary_key=True, auto_created=True, default=uuid.uuid4, editable=False)
+    order = models.ForeignKey(Order)
+    product = models.ForeignKey(Product)
+
+    def __str__(self):
+        return self.order.order_no+":"+self.product.name
+
 
--- oms_app/user/models.py
+++ oms_app/user/models.py
@@ -4,10 +4,10 @@ from django.db import models
 import uuid
 
 
-class user(models.Model):
+class User(models.Model):
     user_id = models.UUIDField(primary_key=True, auto_created=True, default=uuid.uuid4, editable=False)
-    user_name = models.CharField(max_length=64,unique=True,name='user_name')
-    password = models.CharField(max_length=64,name='password')
+    user_name = models.CharField(max_length=64, unique=True, name='user_name')
+    password = models.CharField(max_length=64, name='password')
     create_date = models.DateTimeField(auto_now=True)
     create_by = models.CharField(max_length=64)
     email = models.EmailField()
@@ -15,8 +15,5 @@ class user(models.Model):
     state = models.CharField(max_length=64)
     update_password_date = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
         return self.user_name
-
-
